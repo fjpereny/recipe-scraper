7aa7bcb9d4cd43663f70255a27604cb7b08caad9
Frank Pereny <fjpereny@gmail.com>
2024-01-31 18:49:23 +0800

    Update to make table of contents

--- util/compile_book.py
+++ util/compile_book.py
@@ -14,10 +14,20 @@ if __name__ == '__main__':
 
     with open('Cookbook.tmp', 'w') as cookbook:
         g = glob.glob(dir_path + "/*.html")
+
+        cookbook.write("<h1>Cook Book Table of Contents</h1>")
+        for file in g:
+            recipe_name = file.split('/')[-1].split('.')[0]
+            cookbook.write(f"<h2><a href=\"#{recipe_name}\" >{recipe_name}</a></h2>")
+
+        cookbook.write("<br>")
         for file in g:
             with open(file, 'r') as recipe:
+                recipe_name = file.split('/')[-1].split('.')[0]
                 lines = recipe.read()
+                cookbook.write(f'<div id=\"{recipe_name}\" >')
                 cookbook.write(lines)
+                cookbook.write('</div>')
                 cookbook.write('<br>')
             recipe.close()
     cookbook.close()
